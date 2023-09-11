dc8a43a905f9b85258ebddd52112c9c53b0201a6
Crowdbotics <engineering@crowdbotics.com>
2023-09-11 17:13:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delicate-feather-d-115635",
+  url: "https://delicate-feather-d-115635.botics.co",
+  api: "https://delicate-feather-d-115635.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
